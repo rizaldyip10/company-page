aa328527dafc4a184fc9054478db38d081560531
Rizaldy Iman Putra <rizaldyimanputra@gmail.com>
2024-05-20 15:16:19 +0700

    change image

--- app/(pages)/teams/_components/president-section.tsx
+++ app/(pages)/teams/_components/president-section.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
-import presidentImage from "@/imgs/president2.jpg"
+import presidentImage from "@/imgs/president1.webp"
 import SectionTitle from '@/components/section-title'
 
 const PresidentSection = () => {
